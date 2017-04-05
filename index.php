a36029b0b01fcbeb9694ea0a70f7453c79f4cd17
Sohil Gupta <sohilgupta123@users.noreply.github.com>
2017-04-05 22:14:01 +0530

    Initialization

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/materialize.min.css" type="text/css" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+    </head>
+    <body>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#!" class="brand-logo">Star Security</a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="sass.html">About</a></li>
+                        <li><a href="badges.html">Papers</a></li>
+                        <li><a href="badges.html">Contact Us</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="slider">
+            <ul class="slides">
+                <li>
+                    <img src="images/bg1.jpg"> <!-- random image -->
+                    <div class="caption center-align">
+                        <h3>This is our big Tagline!</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="images/bg2.jpg"> <!-- random image -->
+                    <div class="caption left-align">
+                        <h3>Left Aligned Caption</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    </div>
+                </li>                
+            </ul>
+        </div>
+        <div class="row">
+            <div class="col l3">
+                <div class="feature-icon">
+                    <i class="material-icons">dashboard</i>
+                </div>
+                <div class="feature-title">Visual Editing in Browser</div>
+                <div class="feature-description">Create a website without thinking about content editing. Have content editing layer for free without spending a minute.</div>
+            </div>
+            <div class="col l3">
+                <div class="feature-icon">
+                    <i class="material-icons">subtitles</i>
+                </div>
+                <div class="feature-title">Jekyll and Static Websites</div>
+                <div class="feature-description">We support anything that builds with a Dockerfile, but we optimize
+                    the experience for Jekyll and Static Websites.</div>
+            </div>
+            <div class="col l3">
+                <div class="feature-icon">
+                    <i class="material-icons">loop</i>
+                </div>
+                <div class="feature-title">Upload and sync with GitHub</div>
+                <div class="feature-description">Got a website already? We'll provide the Visual Editing layer then.
+                    Create feature branches and Pull Requests with Visual Editor.</div>                        
+            </div>
+            <div class="col l3">
+                <div class="feature-icon">
+                    <i class="material-icons">airplay</i>
+                </div>
+                <div class="feature-title">Code Editing in Browser</div>
+                <div class="feature-description">Need to do some bigger design changes? Edit all your CSS, HTML and JavaScript files
+                    in your browser.</div>
+            </div>                
+        </div>
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+            <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                        <img src="images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    </div>
+                    <div class="col s10">
+                        <span class="black-text">
+                            This is a square image. Add the "circle" class to it to make it appear circular.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Footer Content</h5>
+                        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 class="white-text">Links</h5>
+                        <ul>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2014 Copyright Text
+                    <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                </div>
+            </div>
+        </footer>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="js/materialize.min.js" ></script>
+        <script>
+            $(document).ready(function () {
+                $('.slider').slider();
+            });
+        </script>
+    </body>
+</html>
